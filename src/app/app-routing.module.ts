83d12bf73e1d14f6503485772a731d9b429532f4
sarakoljic <sk3545@student.uni-lj.si>
2020-11-07 15:30:13 +0100

    neki

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FrontPageComponent } from './components/front-page/front-page.component';
+import { StartGamePopupComponent } from './components/start-game-popup/start-game-popup.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FrontPageComponent,
+  },
+  {
+    path: 'setup',
+    component: StartGamePopupComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
